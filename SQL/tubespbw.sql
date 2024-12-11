066b4b7735f818031a4eb2348de960ce9fefa17a
Jensen Hiem <6182201026@student.unpar.ac.id>
2024-12-11 20:45:06 +0700

    tambah query sql untuk rancangan database

---
+++ SQL/tubespbw.sql
@@ -0,0 +1,91 @@
+DROP TABLE IF EXISTS TransaksiFilm CASCADE;
+DROP TABLE IF EXISTS Transaksi CASCADE;
+DROP TABLE IF EXISTS Reviews CASCADE;
+DROP TABLE IF EXISTS FilmAktor CASCADE;
+DROP TABLE IF EXISTS FilmGenre CASCADE;
+DROP TABLE IF EXISTS Aktor CASCADE;
+DROP TABLE IF EXISTS Genre CASCADE;
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TYPE IF EXISTS rent_enum CASCADE;
+DROP TYPE IF EXISTS role_enum CASCADE;
+DROP TABLE IF EXISTS Film CASCADE;
+
+
+CREATE TYPE role_enum AS ENUM ('Pelanggan', 'Admin');
+CREATE TYPE rent_enum AS ENUM ('Pinjam', 'Pengembalian');
+
+CREATE TABLE Film (
+    idFilm SERIAL PRIMARY KEY,
+    judul VARCHAR(60),
+    stock int,
+    coverFilm VARCHAR(255),
+    hargaPerHari int,
+	deskripsi TEXT,
+	durasi int,
+	rating float
+);
+
+CREATE TABLE Genre (
+    idGenre SERIAL PRIMARY KEY,
+    nama VARCHAR(30)
+);
+ 
+
+CREATE TABLE Aktor (
+    idAktor SERIAL PRIMARY KEY,
+    nama VARCHAR(60),
+    tanggalLahir date,
+    deskripsiDiri TEXT,
+    fotoProfil VARCHAR(255)
+);
+
+CREATE TABLE FilmGenre (
+    idFilm int REFERENCES Film (idFilm) ON DELETE CASCADE,
+    idGenre int REFERENCES Genre (idGenre) ON DELETE CASCADE,
+	PRIMARY KEY (idFilm, idGenre)
+);
+
+CREATE TABLE FilmAktor (
+    idFilm int REFERENCES Film (idFilm) ON DELETE CASCADE,
+    idAktor int REFERENCES Aktor (idAktor) ON DELETE CASCADE,
+	PRIMARY KEY (idFilm, idAktor)
+);
+
+CREATE TABLE Users (
+    idUser SERIAL PRIMARY KEY,
+    nama VARCHAR(60),
+    nomorTelepon VARCHAR (13) UNIQUE,
+    role role_enum DEFAULT 'Pelanggan',
+    password VARCHAR(255)
+);
+
+CREATE TABLE Transaksi (
+	idTransaksi SERIAL PRIMARY KEY,
+    idUser int REFERENCES Users(idUser),
+    tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    tipeTransaksi rent_enum,
+    total int --semua film (termasuk berapa harinya)
+);
+
+CREATE TABLE TransaksiFilm (
+    idTransaksi int REFERENCES Transaksi(idTransaksi) ON DELETE CASCADE,
+    idFilm int REFERENCES Film(idFilm),
+    totalHari int,
+	jumlah int DEFAULT 1,
+    totalHarga int, --1 film berapa hari
+	status VARCHAR(20) DEFAULT 'pending',
+	batasPengembalian date,  --status sama bataspengembalian per film nya
+	PRIMARY KEY (idTransaksi, idFilm)
+);
+
+CREATE TABLE Reviews (
+	idReview SERIAL PRIMARY KEY,
+	idFilm int REFERENCES Film(idFilm),
+	idUser int REFERENCES Users(idUser),
+	rating float CHECK (rating BETWEEN 1 AND 5),
+	komentar TEXT,
+	tanggal TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+--ON DELETE CASCADE buat kalo table parent dihapus, table yang berkaitan (child) bakal kehapus juga
