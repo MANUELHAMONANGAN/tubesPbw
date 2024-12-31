205906b151537d295fd3f5f79879750a70f8d676
Nikolas Giovanni <nikolas.giovanni1@gmail.com>
2024-12-31 20:07:35 +0700

    Fitur: Grafik Penjualan Admin

--- demo/src/main/java/com/example/demo/admin/AdminController.java
+++ demo/src/main/java/com/example/demo/admin/AdminController.java
@@ -1,13 +1,23 @@
 package com.example.demo.admin;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.graph.GraphService;
+import com.example.demo.graph.TransaksiGraph;
+
 @Controller
 public class AdminController {
+    @Autowired
+    private GraphService graphService;
+    
     @GetMapping("/admin/")
     public String index(Model model){
         model.addAttribute("pageSaatIni","home");
@@ -30,5 +40,20 @@ public class AdminController {
         model.addAttribute("currentPage", page);
         model.addAttribute("pageCount", pageCount);
         return "/admin/aktor";
+    }
+
+    @GetMapping("/graph/")
+    public String graph(Model model){
+        List<List<Object>> graphData = new ArrayList<>();
+        graphService.getAllGraphData().forEach(data -> {
+            List<Object> row = new ArrayList<>();
+            row.add(data.getTanggal().toString());
+            row.add(data.getTotal().doubleValue());
+            graphData.add(row);
+        });
+
+        model.addAttribute("graphTitle", "Bulan November");
+        model.addAttribute("graphData", graphData);
+        return "/graph/graph";
     }
 }
