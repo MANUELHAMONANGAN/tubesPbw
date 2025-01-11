9d694f003ab10dc74a16d49d529391079a7933e9
Jensen Hiem <6182201026@student.unpar.ac.id>
2025-01-11 18:23:23 +0700

    tambah cartservice

---
+++ demo/src/main/java/com/example/demo/cart/CartService.java
@@ -0,0 +1,68 @@
+package com.example.demo.cart;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.enums.MethodBayarEnum;
+import com.example.demo.enums.RentEnum;
+import com.example.demo.enums.StatusRent;
+import com.example.demo.transaksi.Transaksi;
+import com.example.demo.transaksi.TransaksiRepository;
+import com.example.demo.transaksiFilm.TransaksiFilm;
+import com.example.demo.transaksiFilm.TransaksiFilmRepository;
+
+@Service
+public class CartService {
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private TransaksiRepository transaksiRepository;
+
+    @Autowired
+    private TransaksiFilmRepository transaksiFilmRepository;
+
+    public List<Cart> getCartByUserId (int idUser) {
+        return cartRepository.findCartByUserId(idUser);
+    }
+
+    public void clearCartByUserId (int idUser) {
+        cartRepository.clearCart(idUser);
+    }
+
+    //bikin transaksi
+    public Transaksi createTransaki (int idUser, List<Cart> cartItems) {
+        if (cartItems == null || cartItems.isEmpty()) {
+            throw new IllegalArgumentException("Cart is empty. Cannot create a transaction");
+        }
+
+        int totalHarga = cartItems.stream().mapToInt(item -> item.getJumlahHari() * item.getHargaPerHari()).sum();
+
+        Transaksi transaksi = new Transaksi();
+        transaksi.setIdUser(idUser);
+        transaksi.setTanggal(new Timestamp(System.currentTimeMillis()));
+        transaksi.setTipeTransaksi(RentEnum.PINJAM);
+        transaksi.setTotal(totalHarga);
+        transaksi.setMetodePembayaran(MethodBayarEnum.NON_TUNAI);
+
+        int idTransaksi = transaksiRepository.save(transaksi);
+
+        for (Cart cartItem : cartItems) {
+            TransaksiFilm transaksiFilm = new TransaksiFilm();
+            transaksiFilm.setIdTransaksi(idTransaksi);
+            transaksiFilm.setIdFilm(cartItem.getIdFilm());
+            transaksiFilm.setTotalHari(cartItem.getJumlahHari());
+            transaksiFilm.setJumlah(1);
+            transaksiFilm.setTotalHarga(cartItem.getJumlahHari() * cartItem.getHargaPerHari());
+            transaksiFilm.setStatus(StatusRent.DRAFT);
+            transaksiFilm.setBatasPengembalian(null);
+
+            transaksiFilmRepository.save(transaksiFilm);
+        }
+
+        return transaksi;
+    }
+}
